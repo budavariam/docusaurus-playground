60d8d594f4d7b6053079c13253014a51419d7c3d
Mátyás Budavári <budavariam@gmail.com>
2022-12-05 08:39:31 +0100

    Update docusaurus config for proper setup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://budavariam.github.io/',
+  baseUrl: '/docusaurus-playground/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
